f0ebf21812ee69d03db215902cd02cd55f74c73e
Dmitry Shelamov <dmitryshelamov@gmail.com>
2020-07-04 15:20:58 +0700

    add serilog

---
+++ eFood/src/Common/eFood.Common/Serilog/SerilogWebHostExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace eFood.Common.Serilog
+{
+    public static class SerilogWebHostExtensions
+    {
+        public static IHostBuilder UseLogging(this IHostBuilder builder, Assembly executingAssembly)
+        {
+            builder.UseSerilog((context, configuration) =>
+            {
+                configuration.ReadFrom.Configuration(context.Configuration, (DependencyContext)null).Enrich
+                    .WithProperty("Application", (object)executingAssembly.GetName().Name, false).Enrich
+                    .WithProperty("Version", (object)executingAssembly.GetName().Version, false).Enrich
+                    .WithMachineName().Enrich.FromLogContext();
+
+                configuration
+                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                    .MinimumLevel.Override("System", LogEventLevel.Warning);
+            });
+
+            return builder;
+        }
+    }
+}
--- eFood/src/Services/eFood.Catalog.WebApi/Program.cs
+++ eFood/src/Services/eFood.Catalog.WebApi/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using eFood.Common.Serilog;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace eFood.Catalog.WebApi
 {
@@ -18,6 +14,7 @@ namespace eFood.Catalog.WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseLogging(Assembly.GetExecutingAssembly())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
