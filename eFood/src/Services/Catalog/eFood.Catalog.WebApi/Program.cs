bf60ee1163ac036d4a1b31233f28d0857e976fed
Dmitry Shelamov <dmitryshelamov@gmail.com>
2020-07-05 12:55:00 +0700

    docker test

--- eFood/src/Services/Catalog/eFood.Catalog.WebApi/Program.cs
+++ eFood/src/Services/Catalog/eFood.Catalog.WebApi/Program.cs
@@ -3,6 +3,7 @@ using eFood.Catalog.WebApi.DAL;
 using eFood.Common.Migrator;
 using eFood.Common.Serilog;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace eFood.Catalog.WebApi
@@ -20,6 +21,13 @@ namespace eFood.Catalog.WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
                 .UseLogging(Assembly.GetExecutingAssembly())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
